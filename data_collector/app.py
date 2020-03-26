2074bc94fa1b2f13761121ec2529a1e0f3e2b963
davidiscarvalho <davidscarvalho11@gmail.com>
2020-03-26 14:48:51 +0000

    webapp collector

--- data_collector/app.py
+++ data_collector/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from send_email import send_email
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
-app.config(['SQLALCHEMY_DATABASE_URL']='postgrassql://postgres:admin@localhost/height_collector')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/height'
 db = SQLAlchemy(app)
 
 
@@ -10,7 +13,7 @@ class Data(db.Model):
     __tablename__ = "data"
     id = db.Column(db.Integer, primary_key=True)
     email_ = db.Column(db.String(120), unique=True)
-    height_ = db.column(db.Integer)
+    height_ = db.Column(db.Integer)
 
     def __init__(self, email_, height_):
         self.email_ = email_
@@ -27,7 +30,18 @@ def success():
     if request.method == 'POST':
         email = request.form["email_name"]
         height = request.form["height_name"]
-        return render_template("sucess.html")
+        print(email, height)
+        if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            average_height = db.session.query(func.avg(Data.height_)).scalar()
+            average_height = round(average_height, 1)
+            count = db.session.query(Data.height_).count()
+            send_email(email, height, average_height, count)
+            print(average_height)
+            return render_template("success.html")
+    return render_template('index.html', text="Seems like we got something from that email once!")
 
 
 if __name__ == '__main__':
--- readme.md
+++ readme.md
@@ -16,19 +16,26 @@ cd ./ghijkl
 #create python virtual environment
 python -m venv .venv
 #activate python virtual environment
-source .venv/bin/activate
+virtual/Scripts/activate
+#in the case it doesn't work :Set-ExecutionPolicy Unrestricted -Force
+install gunicorn
+#generate a requirements.txt
+pip freeze > requirements.txt
 #install requirements
 pip install -r requirements.txt
 
 
 **git**
-
 git push origin master
 
 git rm -r one-of-the-directories
 git commit . -m "Remove directory"
 git push origin master
 
+**heroku**
+heroku create appName
+heroku addons:create heroku-postgresql:hobby-dev --app appName
+heroku config --app appName  #add '?sslmode=require' to the DATABASE URL @script
 
 **notes**
 to convert ipynb
@@ -36,3 +43,9 @@ ipython nbconvert 'scrip.ipynb'
 
 #generate a requirements.txt
 pip freeze > requirements.txt
+
+**build app**
+requirements.txt
+Procfile
+runtime.txt (https://devcenter.heroku.com/articles/python-runtimes#supported-python-runtimes)
+.gitignore
