401034d8611f2fde3ae25cdefd38a57fc2b2e3f1
davidiscarvalho <davidscarvalho11@gmail.com>
2020-10-13 00:21:33 +0100

    13oct

--- social_net/src/posts/models.py
+++ social_net/src/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.core.validators import FileExtensionValidator
+from profiles.models import Profile
 # Create your models here.
+
+
+class Post(models.Model):
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts', validators=(
+        ['png', 'jpg', 'jpeg']), blank=True)
+    liked = models.ManyToManyField(
+        Profiles, default=None, related_name='likes')
