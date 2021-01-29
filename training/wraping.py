43cacba26acef1a8ecafc92dfaf160c6accf2961
davidiscarvalho <davidscarvalho11@gmail.com>
2021-01-29 00:17:02 +0000

    training2021

--- training/wraping.py
+++ training/wraping.py
@@ -1,7 +1,18 @@
 #import examplemod
 #from examplemod import do_a_thing, do_another_thing
-from examplemod import *
+from moddir.examplemod import *
 
 #examplemod.do_a_thing()
 
-do_a_thing()
+#do_a_thing()
+
+
+from colorama import Fore, Back, Style, init
+
+init()
+
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
